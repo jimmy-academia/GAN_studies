ecfcce74e21906434eb0a91dcee68587ece96aa2
jimmy <r06921105@ntu.edu.tw>
2019-02-20 17:01:35 +0800

    DCGAN draft

---
+++ src/DCGAN/README.md
@@ -0,0 +1,17 @@
+# week2 project: DCGAN
+
+goal: use DCGAN to generate mnist and cifar10 dataset, and compare different parameters along the way   
+
+detail:
+
+* /module contains complet DCGAN with different options
+* /experiments contains script to control /module and perform experiments. Results in /experiment/results
+* to train new model use train.py
+* to run experiments or use best produce best result run .py
+
+
+model structure:
+
+
+best result (gif)
+
---
+++ src/DCGAN/module/datafunc.py
@@ -0,0 +1,61 @@
+######################################################
+###  Created by Jimmy Yeh, Made In Taiwan          ###
+###  for the sole purpose of being excellent       ###
+###  What do people usually put here? this doesn't look pretty
+######################################################
+
+# description:
+# in here is dataset(data_dir_root, datatype) for generating datasets
+#     and data_loader(dataset) for creating dataloaders
+#
+#     [usage]
+#     dataset =  dataset(
+#         'path/to/parent_of_dataset_directory', 
+#         'mnist' or 'cifar10')
+#     dataloader = dataloader(dataset)
+
+
+
+import numpy as np
+
+# random_seed = 0
+
+from torchvision import datasets
+from torchvision import transforms
+
+def dataset(data_dir_root, )
+
+def MNIST(data_dir_root, train):
+    normalize = transforms.Normalize((0.1307,), (0.3081,))
+    trans = transforms.Compose([
+        transforms.ToTensor(), normalize,
+    ])
+
+    dataset = datasets.MNIST(
+        data_dir_root+'/mnist', train=train, download=True, transform=trans
+    )
+    return dataset
+
+def CIFAR10(data_dir_root, train):
+    trans = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    # load dataset
+    dataset = datasets.CIFAR10(
+        data_dir_root+'/cifar10/', train=train, download=True, transform=trans
+    )
+    return dataset
+
+def dataloader(dataset):
+
+    data_loader = torch.utils.data.DataLoader(
+        dataset, batch_size=batch_size, sampler=valid_sampler,
+        num_workers=num_workers, pin_memory=pin_memory,
+    )
+    return data_loader
+
+## one line data_loader = torch.utils.data.DataLoader(dataset, batch_size=55, shuffle=True)
